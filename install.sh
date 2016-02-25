9239b02302766c27797c504f2ca96c302a254e16
Chris Kinsman <chris@kinsman.net>
2016-02-25 11:08:41 -0800

    Add disque and remove unneeded utils

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 #base image already has node and nvm installed globally...
 
 echo "================= Updating global nodejs packages ==================="
-sudo npm install -g grunt-cli mocha vows phantomjs casperjs;
-
-echo "================ Installing selenium =================="
-sudo mkdir -p /srv;
-cd /srv && sudo wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar;
+sudo npm install -g gulp;
 
 echo "=============== Installing bower globally ============="
 sudo npm install -g bower
+
+echo "=============== Installing Disque ==============="
+sudo git clone git@github.com:antirez/disque.git
+pushd /disque
+make .
+./src/disque-server
+popd
 
 for file in /u14nod/version/*;
 do
